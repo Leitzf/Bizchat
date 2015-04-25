4cbaa07b72cd5266be75121442bfb8e1c47dba4d
Kdyung <kyung8888@gmail.com>
2015-04-25 16:03:09 -0700

    Chat room shell created, need to check sockets and connectivity.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ CPSC 5910 Project
 Frank Leitz
 Kyra Yung
 Nick Simons
+
+
+Notes:
+node modules folder contains files used in chat application
--- mainchat/index.js
+++ mainchat/index.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('msg sent');
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
