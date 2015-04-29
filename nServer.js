47e877e02f33c06648ec69f11f4065f7d2ab256c
Leitzf <Flink93@yahoo.com>
2015-04-28 22:14:40 -0700

    Added JSON data/fixed some formatting
    +Added example JSON data to be used to prepopulate html files
    +Corrected formatting on a couple of pages so that the navbar doesnt
    look like its moving all over the place
    +Fixed login button. Will now work as long as email format is correct

--- nServer.js
+++ nServer.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var url = require('url');
 var app = express();
+var fs = require('fs');
+var obj;
 
-//serve static content for the app from the 'pages' directory in the app dir
+var jsonObj = require("./data/JSON/user.json");
+
+//serve static content for the app from the 'views' directory in the view
 
 app.use(express.static(__dirname+'/views'));
 app.listen(8000);
+
+
