fdc726342c02e4e01ec12c5f54969074542740b1
Kdyung <kyung8888@gmail.com>
2015-05-07 20:00:42 -0700

    Turned app into single page with script app.js containing routes. All scripts and views in public folder

---
+++ public/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name To Do App
+ * @description
+ * # To do Application
+ *
+ * Main module of the application.
+ */
+ angular
+  .module('BizchatApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/welcome.html'
+	  })
+      .when('/main', {
+        templateUrl: '/mainpageview.html',
+        //controller: 'MainCtrl'
+      })
+      .when('/roomlist/:userId', {
+        templateUrl: '/userroomlistview.html',
+        //controller: 'RoomsCtrl'
+      })
+      .when('/roomlist/:userId/edit/:roomID', {
+        templateUrl: '/roomeditview.html',
+        //controller: 'RoomsCtrl'
+      })
+      .when('/mainchat', {
+        templateUrl: '/mainchatview.html',
+        //controller: 'RoomsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
