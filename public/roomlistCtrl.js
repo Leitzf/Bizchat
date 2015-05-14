4544bb7205e234fbe350e130064e05e1b9b3e6ee
Kdyung <kyung8888@gmail.com>
2015-05-14 10:39:09 -0700

    Formatted room list and ctrl

--- public/roomlistCtrl.js
+++ public/roomlistCtrl.js
@@ -9,29 +9,28 @@
  */
 angular
 .module('BizchatApp')
-.controller('RoomListCtrl', function ($scope, $http) {
- 	/*
- 	$http.get('publicroomlist.json').success(function(data) {
- 		console.log("Successfully acquired room data");
-		$scope.roomlist = data;
-	});
-	*/
-	$scope.getRoomList = function() {
-		$http.get('/data/roomlist.json').success(function(data, status, headers, config) {
-			console.og("Success Roomlist acquired");
-		   $scope.results = data;
-		   var roomlist = [];
-		   for (var i = 0; i < data.length; i++) {
-			   roomlist[i] = { 
-			   		"Name": data[i].Name,
-			   		"Description": data[i].Description,
-			   		"DateDestroy": data[i].DateDestroy 
-			   	};
-		   }
-		   $rootScope.roomlist = roomlist;
-		}).error(function(data, status, headers, config) {
-  			console.log("Error acquiring Room data");
-  			return;
-		});
-	};
-});
+.controller('RoomListCtrl', ['$scope', '$rootScope', '$http',
+	function($scope,  $rootScope, $http) {
+		$scope.getRoomList = function() {
+
+			$http.get('publicroomlist.json').success(function(data, status, headers, config) {
+				console.log("Success Roomlist acquired");
+			   $scope.results = data;
+			   
+			   var roomlist = [];
+			   for (var i = 0; i < data.length; i++) {
+				   roomlist[i] = { 
+				   		"Name": data[i].Name,
+				   		"Description": data[i].Description,
+				   		"DateDestroy": data[i].DateDestroy 
+				   	};
+			   }
+			   $rootScope.roomlist = roomlist;
+
+			}).error(function(data, status, headers, config) {
+	  			console.log("Error acquiring Room data");
+	  			return;
+			});
+		};
+	}
+]);
