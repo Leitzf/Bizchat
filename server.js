10929b6edae5a740da6c9f5b89a19e71bb07bc53
Leitzf <Flink93@yahoo.com>
2015-06-03 22:48:49 -0700

    Auth tests

--- server.js
+++ server.js
@@ -297,7 +297,7 @@ app.get('/auth/facebook/callback',
 //   login page.  Otherwise, the primary route function function will be called, 
 //   which, in this example, will redirect the user to the home page. 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  passport.authenticate('facebook', { successFlash: 'Welcome!' }),
   function(req, res) {
     res.redirect('/');
   });
