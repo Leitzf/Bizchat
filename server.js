7e7e180c8dacdf7f82f1c717500185b94f1dbead
Leitzf <Flink93@yahoo.com>
2015-06-04 15:27:27 -0700

    Removed print facebook profile

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      console.log(profile);
       // To keep the example simple, the user's Windows Live profile is returned
       // to represent the logged-in user.  In a typical application, you would
       // want to associate the Windows Live account with a user record in your
