d005090c4b30522416f99d561ca30bf3f8fe3eb5
Kdyung <kyung8888@gmail.com>
2015-06-04 23:41:42 -0700

    rebased git and made some edits. User routes in server.js might need looking at. Good job everyone.

--- server.js
+++ server.js
@@ -277,12 +277,7 @@ app.get('/rooms/:roomId', function (req, res) {
 	else {
 	  res.redirect('/#/')
 	}
-<<<<<<< HEAD
 	*/	
-=======
-	*/
-	
->>>>>>> 332663c... removed authentification non-secure handling
 });
 /*
 app.get('/room/join/:roomId', function (req, res) {
