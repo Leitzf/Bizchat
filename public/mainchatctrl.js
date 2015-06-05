dddcf7f107faa7be29697e87206583d0bb51bd6d
Kdyung <kyung8888@gmail.com>
2015-06-04 23:30:34 -0700

    console log
    Conflicts:
    	public/mainchatctrl.js

--- public/mainchatctrl.js
+++ public/mainchatctrl.js
@@ -50,6 +50,7 @@ angular
 				    //});
 				}
 				console.log("messagelist"+ messagelist);
+
 				$rootScope.messagelist = messagelist;
 			}).error(function(data, status, headers, config) {
 	  			console.log("Error acquiring Message data");
